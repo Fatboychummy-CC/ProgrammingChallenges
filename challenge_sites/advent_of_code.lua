f01417b92ac3d39c26ce0e5296351b09e7b8c166
Matthew W <fatboychummy@gmail.com>
2024-12-01 23:39:28 -0700

    Catch "other person's result" error

--- challenge_sites/advent_of_code.lua
+++ challenge_sites/advent_of_code.lua
@@ -229,6 +229,9 @@ function site.submit(challenge, challenge_answer, challenge_year, challenge_day,
 
   -- Check if the submission was successful.
   local message = response_text:match("<article><p>(.-) If you're stuck.*</p></article>")
+  if not message then
+    message = response_text:match("<article><p>(.-)Curiously,</p></article>") -- "Curiously, it's the right answer for someone else"
+  end
   local correct = response_text:match("That's the right answer!")
 
   if correct then
