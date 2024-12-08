3f9c2b09eb4cb178d899f32573552812178a1d69
Matthew W <fatboychummy@gmail.com>
2024-12-08 02:16:04 -0700

    Add table utility functions for permutations and combinations

--- libs/table_utils.lua
+++ libs/table_utils.lua
@@ -53,4 +53,68 @@ function table_utils.contains(t, ...)
   return false
 end
 
+--- Permute a table.
+--- This will return a table of all possible permutations of the input table.
+---@param t table The table to permutate.
+---@return any[] permutations The permutations of the table.
+function table_utils.permutate(t)
+  if #t == 0 then
+    return {}
+  end
+
+  if #t == 1 then
+    return {t}
+  end
+
+  local permutations = {}
+
+  for i = 1, #t do
+    local copy = {}
+    for j = 1, #t do
+      if j ~= i then
+        table.insert(copy, t[j])
+      end
+    end
+
+    local sub_permutations = table_utils.permutate(copy)
+
+    for _, p in ipairs(sub_permutations) do
+      table.insert(p, 1, t[i])
+      table.insert(permutations, p)
+    end
+  end
+
+  return permutations
+end
+
+--- Calculate all possible combinations of a table (including smaller subsets).
+---@param t table The table to calculate combinations for.
+---@param size integer? The size of the combinations to return. If nil, returns ALL combinations.
+---@return any[] combinations The combinations of the table.
+function table_utils.combinations(t, size)
+  local function generate_combinations(t, size, start, current_combination, all_combinations)
+    if size == 0 then
+      table.insert(all_combinations, {table.unpack(current_combination)})
+      return
+    end
+
+    for i = start, #t do
+      table.insert(current_combination, t[i])
+      generate_combinations(t, size - 1, i + 1, current_combination, all_combinations)
+      table.remove(current_combination)
+    end
+  end
+
+  local combinations = {}
+  if size then
+    generate_combinations(t, size, 1, {}, combinations)
+  else
+    for s = 1, #t do
+      generate_combinations(t, s, 1, {}, combinations)
+    end
+  end
+
+  return combinations
+end
+
 return table_utils
